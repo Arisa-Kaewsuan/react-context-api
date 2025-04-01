9dd6c65849bec029c6e254afb6b11b7b07a097aa
MINT <mint_chanit@hotmail.com>
2025-04-01 13:31:49 +0700

    feat: add UserDataContext for managing user state

--- client/src/pages/ViewProductPage.jsx
+++ client/src/pages/ViewProductPage.jsx
@@ -1,7 +1,12 @@
 import { useNavigate } from "react-router-dom";
+import { UserDataContext } from "../App";
+import { useContext } from "react";
 
 function ViewProductPage() {
+  const context = useContext(UserDataContext);
+
   const navigate = useNavigate();
+
   return (
     <div>
       <h1>View Product Page</h1>
@@ -11,7 +16,7 @@ function ViewProductPage() {
       </div>
 
       <div className="product-promotion-box">
-        <h2>คุณคือสมาชิกในระดับ (x) ดังนั้นคุณได้สิทธิพิเศษลด 50%</h2>
+        <h2>คุณคือสมาชิกในระดับ ({context.userData.level}) ดังนั้นคุณได้สิทธิพิเศษลด 50%</h2>
         <button onClick={() => alert("🥳🥳🥳🥳")}>กดรับสิทธิ</button>
       </div>
 
